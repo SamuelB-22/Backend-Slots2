f7016ea231543f4f84c7c294029cd379fd51348b
SamuelB-22 <samuel.bastidasp@comunidad.iush.edu.co>
2024-11-12 18:45:52 -0500

    d

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(
   'slot',
   'root',
-  'Elefante1.',
+  '12345678.',
   {
     host: 'localhost',
     dialect: 'mysql' 
